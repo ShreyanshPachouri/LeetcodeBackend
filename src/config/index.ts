47c2f0ef3531d98c62983a05567d5a0962f26ca2
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2026-09-10 19:31:50 +0530

    initial commit and added problem schema

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,8 @@
 import dotenv from 'dotenv';
 
 type ServerConfig = {
-    PORT: number
+    PORT: number,
+    DB_URL: string
 }
 
 function loadEnv() {
@@ -13,5 +14,6 @@ function loadEnv() {
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+    PORT: Number(process.env.PORT) || 3001,
+    DB_URL: process.env.DB_URL || 'mongodb+srv://pachourishreyansh_db_user:HK2IpKrlXuJ1FjVA@cluster0.yfuzrou.mongodb.net/?appName=Cluster0'
 };
