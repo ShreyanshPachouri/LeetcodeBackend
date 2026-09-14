c022722b9739bf61559522709eaf5ce867900e60
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2026-09-14 15:26:13 +0530

    fixes

--- SubmissionService/src/services/submission.service.ts
+++ SubmissionService/src/services/submission.service.ts
@@ -1,90 +1,91 @@
 import { getProblemById } from "../apis/problem.api";
 import logger from "../config/logger.config";
-import { ISubmission, SubmissionStatus } from "../models/submission.model";
+import { ISubmission, ISubmissionData, SubmissionStatus } from "../models/submission.model";
 import { addSubmissionJob } from "../producers/submission.producer";
 import { ISubmissionRepository } from "../repositories/submission.repository";
 import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
 
-export interface ISubmissionService{
-    createSubmission(submissionData: Partial<ISubmission>): Promise<ISubmission>
-    getSubmissionById(id: string): Promise<ISubmission | null>
-    getSubmissionByProblemId(problemId: string): Promise<ISubmission[]>
-    deleteSubmissionById(id: string): Promise<Boolean>
-    updateSubmissionStatus(id: string, status: SubmissionStatus): Promise<ISubmission | null>
+export interface ISubmissionService {
+    createSubmission(submissionData: Partial<ISubmission>): Promise<ISubmission>;
+    getSubmissionById(id: string): Promise<ISubmission | null>;
+    getSubmissionsByProblemId(problemId: string): Promise<ISubmission[]>;
+    deleteSubmissionById(id: string): Promise<boolean>;
+    updateSubmissionStatus(id: string, status: SubmissionStatus, submissionData: ISubmissionData): Promise<ISubmission | null>;
 }
 
-export class SubmissionService implements ISubmissionService{
-    private submissionRepository: ISubmissionRepository
+export class SubmissionService implements ISubmissionService {
 
-    constructor(submissionRepository: ISubmissionRepository){
-        this.submissionRepository = submissionRepository
+    private submissionRepository: ISubmissionRepository;
+
+    constructor(submissionRepository: ISubmissionRepository) {
+        this.submissionRepository = submissionRepository;
     }
 
     async createSubmission(submissionData: Partial<ISubmission>): Promise<ISubmission> {
-        if(!submissionData.problemId){
-            throw new BadRequestError("Problem ID is required")
+        // check if the problem exists
+        if(!submissionData.problemId) {
+            throw new BadRequestError("Problem ID is required");
         }
 
-        if(!submissionData.code){
-            throw new BadRequestError("Code is required")
+        if(!submissionData.code) {
+            throw new BadRequestError("Code is required");
         }
 
-        if(!submissionData.language){
-            throw new BadRequestError("Language is required")
+        if(!submissionData.language) {
+            throw new BadRequestError("Language is required");
         }
 
-        const problem = await getProblemById(submissionData.problemId)
-
-        if(!problem){
-            throw new NotFoundError("Problem not found")
+        logger.info("Getting problem by ID", { problemId: submissionData.problemId });
+        const problem = await getProblemById(submissionData.problemId);
+        if(!problem) {
+            throw new NotFoundError("Problem not found or something went wrong");
         }
 
-        const submission = await this.submissionRepository.create(submissionData)
+        // add the submission payload to the db
 
+        const submission = await this.submissionRepository.create(submissionData);
+
+        // submission to redis queue
         const jobId = await addSubmissionJob({
-            submissionId: submission._id.toString(),
+            submissionId: submission.id,
             problem,
             code: submissionData.code,
             language: submissionData.language
-        })
+        });
 
-        logger.info(`Submission job added with ${jobId}`)
+        logger.info(`Submission job added: ${jobId}`);
 
-        return submission
+        return submission;
     }
 
     async getSubmissionById(id: string): Promise<ISubmission | null> {
-        const submission = await this.submissionRepository.findById(id)
-
-        if(!submission){
-            throw new NotFoundError("Submission not found")
+        const submission = await this.submissionRepository.findById(id);
+        if(!submission) {
+            throw new NotFoundError("Submission not found");
         }
-
-        return submission
+        return submission;
     }
 
-    async getSubmissionByProblemId(problemId: string): Promise<ISubmission[]> {
-        const submissions = await this.submissionRepository.findByProblemId(problemId)
-        return submissions
+    async getSubmissionsByProblemId(problemId: string): Promise<ISubmission[]> {
+        const submissions = await this.submissionRepository.findByProblemId(problemId);
+        return submissions;
     }
 
-    async deleteSubmissionById(id: string): Promise<Boolean> {
-        const submission = await this.submissionRepository.deleteById(id)
-
-        if(!submission){
-            throw new NotFoundError("Submission not found")
+    async deleteSubmissionById(id: string): Promise<boolean> {
+        const result = await this.submissionRepository.deleteById(id);
+        if(!result) {
+            throw new NotFoundError("Submission not found");
         }
-
-        return submission
+        return result;
     }
 
-    async updateSubmissionStatus(id: string, status: SubmissionStatus): Promise<ISubmission | null> {
-        const submission = await this.submissionRepository.updateStatus(id, status)
-
-        if(!submission){
-            throw new NotFoundError("Submission not found")
+    async updateSubmissionStatus(id: string, status: SubmissionStatus, submissionData: ISubmissionData): Promise<ISubmission | null> {
+        const submission = await this.submissionRepository.updateStatus(id, status, submissionData);
+        if(!submission) {
+            throw new NotFoundError("Submission not found");
         }
-
-        return submission
+        return submission;
     }
+    
+    
 }
